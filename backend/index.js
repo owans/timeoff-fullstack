632ade214868adfd1e34c65e129da4cc0887ae1c
Owanate Amachree <amachreeowanate@gmail.com>
2019-04-29 09:45:38 -0400

    fix timestring function

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(cors());
 
 //logger middleware
 app.use((req, res,next) => {
-    console.log(`[${new Date().toTimeString}]: ${req.method} ${req.url}`)
+    console.log(`[${new Date().toTimeString()}]: ${req.method} ${req.url}`)
     next();
 })
 
